c70efb2454ad383095877a78377e89d8e92d38d8
Gaurav Oberoi <goberoi@gmail.com>
2016-08-23 22:15:12 -0700

    New version with async support, and checking for job status.

--- google_speech.py
+++ google_speech.py
@@ -1,97 +1,81 @@
-"""Google Cloud Speech API sample application using the REST API for batch
-processing."""
-
 import argparse
 import base64
 import json
-import sys
+import time
+import os
+from datetime import datetime
 
 from googleapiclient import discovery
 import httplib2
 from oauth2client.client import GoogleCredentials
 
 
-DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
-                 'version={apiVersion}')
+# Global internal vars
+_SPEECH_SERVICE = None
 
-API_KEY = "AIzaSyALQzz-AS-67fx3AUR45Q9ZmWI9F_L821o"
 
+# Set an API KEY to the environment variable GOOGLE_API_KEY
 def get_speech_service():
-#    credentials = GoogleCredentials.get_application_default().create_scoped(
-#        ['https://www.googleapis.com/auth/cloud-platform'])
-#    credentials.authorize(http)
+    global _SPEECH_SERVICE
+    if not _SPEECH_SERVICE:
+        _SPEECH_SERVICE =  discovery.build(
+            'speech',
+            'v1beta1',
+            http=httplib2.Http(),
+            developerKey=os.environ['GOOGLE_API_KEY'])
+    return _SPEECH_SERVICE
 
-    http = httplib2.Http()
-    return discovery.build(
-        'speech', 'v1beta1', http=http, discoveryServiceUrl=DISCOVERY_URL, developerKey=API_KEY)
 
-
-def parse_file(speech_file):
-    """Transcribe the given audio file.
-
+def process_speech_uri(speech_uri):
+    """Transcribe the given audio file uri asynchronously.
     Args:
-        speech_file: the name of the audio file.
+        speech_uri: the audio file uri.
     """
 
-    print("START: open file and base64 decode")
-    with open(speech_file, 'rb') as speech:
-        speech_content = base64.b64encode(speech.read())
-    print("END: open file and base64 decode")        
-
-    service = get_speech_service()
-
-    print("START: setup request and decode content as UTF8")
-    content = speech_content.decode('UTF-8')
-    print("Size in bytes: ", sys.getsizeof(content))
-    print("Size in megabytes: ", (sys.getsizeof(content) / 1000000.0))
-
-    service_request = service.speech().asyncrecognize(
-        body={
-            'config': {
-                'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
-                'sampleRate': 16000,  # 16 khz
-                'languageCode': 'en-US',  # a BCP-47 language tag
-            },
-            'audio': {
-                'content': content
-                }
-            })
-    print("END: setup request and decode content as UTF8")
-
-    print("START: make the request")
-    response = service_request.execute()
-    print("END: make the request")
-
-    print("START: dump response to JSON and print")
-    print(json.dumps(response))
-    print("END: dump response to JSON and print")
-
-def parse_uri(speech_file):
     service = get_speech_service()
     service_request = service.speech().asyncrecognize(
         body={
             'config': {
-                'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
-                'sampleRate': 16000,  # 16 khz
-                'languageCode': 'en-US',  # a BCP-47 language tag
+                'encoding': 'LINEAR16',
+                'sampleRate': 44100, 
+                'languageCode': 'en-US',
             },
             'audio': {
-                'uri' : speech_file
+                'uri' : speech_uri
                 }
             })
-
-    print("START: make the request")
     response = service_request.execute()
-    print("END: make the request")
 
-    print("START: dump response to JSON and print")
     print(json.dumps(response))
-    print("END: dump response to JSON and print")
+
+    fetch_job_result(response['name'])
+
+    
+def fetch_job_result(name):
+    service = get_speech_service()
+    service_request = service.operations().get(name=name)
+
+    while True:
+        # Give the server a few seconds to process.
+        print('%s, waiting for results from job, %s' % (datetime.now().replace(second=0, microsecond=0), name))
+        time.sleep(15)
+        # Get the long running operation with response.
+        response = service_request.execute()
+
+        if 'done' in response and response['done']:
+            break
+
+    print(json.dumps(response))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'speech_file', help='Full path of audio file to be recognized')
+    parser.add_argument('-u','--uri', required=False, help='URI of audio file. Must be hosted on Google, e.g.: gs://example-content/ben-podcast-waze.flac')
+    parser.add_argument('-n','--name', required=False, help='Name of a job that is in progress.')
     args = parser.parse_args()
-#    parse_file(args.speech_file)
-    parse_uri(args.speech_file)
+    if args.uri:
+        process_speech_uri(args.uri)
+    elif args.name:
+        fetch_job_result(args.name)
+    else:
+        parser.print_help()
